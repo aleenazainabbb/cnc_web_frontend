7ef233e2e07ffc8bb057a734859ace62b486b1c2
Dev1 <dev1@marifahsol.onmicrosoft.com>
2025-05-29 06:00:11 -0700

    profile completed

--- src/components/Booking/profile.tsx
+++ src/components/Booking/profile.tsx
@@ -3,45 +3,44 @@ import React from 'react';
 import styles from './styles/profile.module.css';
 
 export default function Profile() {
-  return (
-    <div className={styles.profileFormContainer}>
-      <div className={`${styles.profileFormRow} ${styles.profileFormRowLg}`}>
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
+                {/* First Row: First & Last Name */}
+                <div className={styles.profileFormRow}>
+                    <div>
+                        <label className={styles.label}>First Name</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            placeholder="Enter first name"
+                        />
+                    </div>
+                    <div>
+                        <label className={styles.label}>Last Name</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            placeholder="Enter last name"
+                        />
+                    </div>
+                </div>
 
-        {/* First Name */}
-        <div className={styles.profileFormFlex1}>
-          <label className={styles.profileFormLabel}>First Name</label>
-          <input
-            type="text"
-            className={styles.profileFormInput}
-            placeholder="Enter first name"
-          />
+                {/* Second Row: Email & Button */}
+                <div className={styles.profileFormRow}>
+                    <div>
+                        <label className={styles.label}>Email Address</label>
+                        <input
+                            type="email"
+                            className={styles.input}
+                            placeholder="Please enter email address"
+                        />
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.button}>Update</button>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        {/* Last Name */}
-        <div className={styles.profileFormFlex1}>
-          <label className={styles.profileFormLabel}>Last Name</label>
-          <input
-            type="text"
-            className={styles.profileFormInput}
-            placeholder="Enter last name"
-          />
-        </div>
-      </div>
-
-      {/* Email */}
-      <div className={styles.profileFormRow}>
-        <label className={styles.profileFormLabel}>Email Address</label>
-        <input
-          type="email"
-          className={styles.profileFormInput}
-          placeholder="Enter email"
-        />
-      </div>
-
-      {/* Update Button */}
-      <div className={styles.profileFormButtonContainer}>
-        <button className={styles.profileFormButton}>Update</button>
-      </div>
-    </div>
-  );
+    );
 }
